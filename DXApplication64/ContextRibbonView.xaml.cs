c62eca843761da6356a77c2040cfdc23953ba474
Florian Koch <floriankochde@arcor.de>
2018-08-06 07:50:28 +0200

    modified sample

---
+++ DXApplication64/ContextRibbonView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace DXApplication64
+{
+    public partial class ContextRibbonView
+    {
+        public ContextRibbonView()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty CategoryCaptionProperty = DependencyProperty.Register(
+            nameof(CategoryCaption),
+            typeof(String),
+            typeof(ContextRibbonView));
+
+        public String CategoryCaption { get; set; }
+    }
+}
---
+++ DXApplication64/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Ribbon;
+
+namespace DXApplication64
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class RibbonMerger : DependencyObject
+    {
+        public static readonly DependencyProperty ChildRibbonsProperty = DependencyProperty.RegisterAttached(
+            "ChildRibbons",
+            typeof(RibbonCollection),
+            typeof(RibbonMerger),
+            new PropertyMetadata(ChildRibbonsPropertyChangedCallback)
+            );
+
+        private static void ChildRibbonsPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is RibbonControl ribbon))
+            {
+                throw new InvalidOperationException("This attached property is working exclusively with RibbonControls!");
+            }
+            if (e.OldValue is RibbonCollection oldCollection)
+            {
+                foreach (var childRibbon in oldCollection)
+                {
+                    ribbon.UnMerge(childRibbon);
+                }
+            }
+
+            if (e.NewValue is RibbonCollection newCollection)
+            {
+                foreach (var childRibbon in newCollection)
+                {
+                    ribbon.Merge(childRibbon);
+                }
+            }
+        }
+
+        public static RibbonCollection GetChildRibbons(RibbonControl target)
+        {
+            return (RibbonCollection)target.GetValue(ChildRibbonsProperty);
+        }
+
+        public static void SetChildRibbons(RibbonControl target, RibbonCollection value)
+        {
+            target.SetValue(ChildRibbonsProperty, value);
+        }
+    }
+
+    public class RibbonCollection : List<RibbonControl>
+    { }
+}
